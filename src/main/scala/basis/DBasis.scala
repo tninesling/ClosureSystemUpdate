f37ecb1922dfc8a67348587230d273613dadd8b9
Taylor Ninesling <t9sling@hotmail.com>
2017-09-13 23:38:35 +0200

    Completed initial D basis update implementation

--- src/main/scala/basis/DBasis.scala
+++ src/main/scala/basis/DBasis.scala
@@ -17,14 +17,16 @@ class DBasis extends Basis {
   // Step 2 in D-basis update section of paper
 
   /** For sets x and y greaterThanOrEqualTo(x, y) is True if x -> y is in Sigma^b (binary) */
-  def greaterThanOrEqualTo(x: Set[String], y: Set[String]): Boolean = binary.contains(Implication(x, y))
+  def greaterThanOrEqualTo(x: Set[String], y: Set[String]) = binary contains Implication(x, y)
 
-  def min(x: Set[String], y: Set[String]): Set[String] = {
+  def greaterThanOrEqualTo(x: Set[String], y: Set[String], newSet: Set[String]) =
+    (binary & unbrokenImplications(newSet)) contains Implication(x, y)
+
+  def min(x: Set[String], y: Set[String]): Set[String] =
     if (greaterThanOrEqualTo(x, y))
       y
     else
       x
-  }
 
   def A_x(x: Set[String], newSet: Set[String]): Set[Set[String]] = {
     val allPossible =
@@ -41,23 +43,17 @@ class DBasis extends Basis {
     if (imp.premise.forall(elem => !targets(newSet).contains(elem))) // no element of C is in the target set
       Set(imp)
     else { // some element of C is in the target set
-      val C = imp.premise
-      val removables =
-        (C & targets(newSet)).subsets
-          .toSet
-          .filter(_.size > 0) // Remove Set()
-
-      removables.map(removable =>
-        Implication(((C &~ removable) | removable.flatMap(x => A_x(Set(x), newSet).flatten)), imp.conclusion)
-      )
+      for {
+        removable <- imp.premise & targets(newSet)
+        replacement <- A_x(Set(removable), newSet)
+      } yield Implication(((imp.premise - removable) | replacement), imp.conclusion)
     }
   }
 
   // Step 3 of D-basis update section of paper
 
   // Calculates the ideal of an element in the lattice
-  def ideal(a: Set[String]): Set[Set[String]] =
-    ideal(a, Set())
+  def ideal(a: Set[String]): Set[Set[String]] = ideal(a, Set())
 
   // Calculates the ideal of an element in the lattice after partial orders are broken by newSet
   def ideal(a: Set[String], newSet: Set[String]): Set[Set[String]] = {
@@ -69,10 +65,56 @@ class DBasis extends Basis {
       .foldLeft(children)((x,y) => x | y)
   }
 
+  // If imp1 << imp2, true otherwise false
+  def refines(imp1: Implication, imp2: Implication): Boolean = refines(imp1, imp2, Set())
+
+  def refines(imp1: Implication, imp2: Implication, newSet: Set[String]): Boolean =
+    if (imp1.conclusion.equals(imp2.conclusion))
+      imp1.premise.subsetOf(imp2.premise | ideal(imp2.premise, newSet).flatten)
+    else
+      false
+
   def restrictedAlift(imp: Implication, newSet: Set[String]): Set[Implication] = {
-    val lifted = Alift(imp, newSet) // TO-DO
-    lifted
+    val lifted = Alift(imp, newSet)
+    val candidates = nonBinary | lifted
+    lifted.filterNot(Y => (candidates - Y).exists(Z => refines(Z, Y, newSet)))
   }
 
-  def update(closedSet: Set[String]) = {}
+  // Step 4
+  def upSet(A: Set[String], newSet: Set[String]): Set[Set[String]] =
+    A map (a =>
+      this.baseSet filter (x =>
+        greaterThanOrEqualTo(Set(x), Set(a), newSet)
+      )
+    )
+
+  def minimalElems(A: Set[Set[String]], newSet: Set[String]) =
+    A filterNot (a =>
+      (A - a) exists (x => greaterThanOrEqualTo(a, x, newSet))
+    )
+
+  def update(newSet: Set[String]) = {
+    val broken = brokenImplications(newSet)
+    val unbroken = unbrokenImplications(newSet)
+    val lifted = broken flatMap (imp => restrictedAlift(imp, newSet))
+
+    // Perform body building on lifted broken implications
+    val updated = lifted flatMap {imp =>
+      val extensions = this.baseSet &~ (newSet | imp.conclusion | minimalElems(upSet(imp.conclusion, newSet), newSet).flatten)
+      extensions map (ext =>
+        Implication(imp.premise + ext, imp.conclusion)
+      )
+    }
+
+    val unrefinedCandidates = updated | unbroken
+
+    // Filter out implications which have refinements
+    val refined = unrefinedCandidates filterNot {imp1 =>
+      (unrefinedCandidates - imp1) exists {imp2 =>
+        refines(imp2, imp1, newSet)
+      }
+    }
+
+    this.basis = refined
+  }
 }
--- src/test/scala/basis/DBasisSpec.scala
+++ src/test/scala/basis/DBasisSpec.scala
@@ -17,8 +17,27 @@ class DBasisSpec extends FlatSpec with Matchers {
 
     testBasis.targets(newSet) should equal (Set("x", "y"))
   }
+  it should "be 4" in {
+    val testBasis = new DBasis
+    testBasis.baseSet = Set("1", "2", "3", "4", "5")
+    testBasis.basis = Set(
+      Implication(Set("5"), Set("4")),
+      Implication(Set("2", "3"), Set("4")),
+      Implication(Set("2", "4"), Set("3")),
+      Implication(Set("3", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("3")),
+      Implication(Set("1", "4"), Set("5")),
+      Implication(Set("2", "5"), Set("1")),
+      Implication(Set("3", "5"), Set("1")),
+      Implication(Set("1", "2", "3"), Set("5"))
+    )
+    val newSet = Set("5")
 
-  "The A-lift" should "produce {a1,y}->d, {x,a2}->d, and {a1,a2}->d" in {
+    testBasis.targets(newSet) should equal (Set("4"))
+  }
+
+  "The A-lift" should "produce {a1,y}->d and {x,a2}->d" in {
     val testBasis = new DBasis
     testBasis.baseSet = Set("x", "y", "d", "a1", "a2")
     testBasis.basis = Set(
@@ -28,7 +47,26 @@ class DBasisSpec extends FlatSpec with Matchers {
     )
     val newSet = Set("a1", "a2")
 
-    testBasis.Alift(Implication(Set("x", "y"), Set("d")), newSet) should equal (Set(Implication(Set("a1", "y"), Set("d")), Implication(Set("x", "a2"), Set("d")), Implication(Set("a1", "a2"), Set("d"))))
+    testBasis.Alift(Implication(Set("x", "y"), Set("d")), newSet) should equal (Set(Implication(Set("a1", "y"), Set("d")), Implication(Set("x", "a2"), Set("d"))))
+  }
+  it should "be empty" in {
+    val testBasis = new DBasis
+    testBasis.baseSet = Set("1", "2", "3", "4", "5")
+    testBasis.basis = Set(
+      Implication(Set("5"), Set("4")),
+      Implication(Set("2", "3"), Set("4")),
+      Implication(Set("2", "4"), Set("3")),
+      Implication(Set("3", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("3")),
+      Implication(Set("1", "4"), Set("5")),
+      Implication(Set("2", "5"), Set("1")),
+      Implication(Set("3", "5"), Set("1")),
+      Implication(Set("1", "2", "3"), Set("5"))
+    )
+    val newSet = Set("5")
+
+    testBasis.Alift(Implication(Set("2", "5"), Set("1")), newSet) should equal (Set(Implication(Set("2", "5"), Set("1"))))
   }
 
   // Example 4.3
@@ -50,4 +88,76 @@ class DBasisSpec extends FlatSpec with Matchers {
     (a3DownA &~ yDown) should equal (Set(Set("a2")))
   }
 
+  "The update method" should "add implications 15 -> 4, 25 -> 4, and 35 -> 4" in {
+    val testBasis = new DBasis
+    testBasis.baseSet = Set("1", "2", "3", "4", "5")
+    testBasis.basis = Set(
+      Implication(Set("5"), Set("4")),
+      Implication(Set("2", "3"), Set("4")),
+      Implication(Set("2", "4"), Set("3")),
+      Implication(Set("3", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("3")),
+      Implication(Set("1", "4"), Set("5")),
+      Implication(Set("2", "5"), Set("1")),
+      Implication(Set("3", "5"), Set("1")),
+      Implication(Set("1", "2", "3"), Set("5"))
+    )
+    val newSet = Set("5")
+
+    testBasis.update(newSet)
+
+    val updatedBasis = Set(
+      Implication(Set("1", "5"), Set("4")),
+      Implication(Set("2", "5"), Set("4")),
+      Implication(Set("3", "5"), Set("4")),
+      Implication(Set("2", "3"), Set("4")),
+      Implication(Set("2", "4"), Set("3")),
+      Implication(Set("3", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("3")),
+      Implication(Set("1", "4"), Set("5")),
+      Implication(Set("2", "5"), Set("1")),
+      Implication(Set("3", "5"), Set("1")),
+      Implication(Set("1", "2", "3"), Set("5"))
+    )
+
+    testBasis.basis should equal (updatedBasis)
+  }
+  it should "remove 23->4 and 123->5" in {
+    val testBasis = new DBasis
+    testBasis.baseSet = Set("1", "2", "3", "4", "5")
+    testBasis.basis = Set(
+      Implication(Set("5"), Set("4")),
+      Implication(Set("2", "3"), Set("4")),
+      Implication(Set("2", "4"), Set("3")),
+      Implication(Set("3", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("3")),
+      Implication(Set("1", "4"), Set("5")),
+      Implication(Set("2", "5"), Set("1")),
+      Implication(Set("3", "5"), Set("1")),
+      Implication(Set("1", "2", "3"), Set("5"))
+    )
+    val newSet = Set("1", "2", "3")
+
+    testBasis.update(newSet)
+
+    val updatedBasis = Set(
+      Implication(Set("5"), Set("4")),
+      Implication(Set("2", "4"), Set("3")),
+      Implication(Set("3", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("2")),
+      Implication(Set("1", "4"), Set("3")),
+      Implication(Set("1", "4"), Set("5")),
+      Implication(Set("2", "5"), Set("1")),
+      Implication(Set("3", "5"), Set("1"))
+    )
+
+    println("Update with 123:")
+    println(testBasis.basis &~ updatedBasis)
+    println()
+    println(updatedBasis &~ testBasis.basis)
+    testBasis.basis should equal (updatedBasis)
+  }
 }
